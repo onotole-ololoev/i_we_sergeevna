29c0c8ac3850b40f748dc606b035421bdddc4afc
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-07-14 10:36:45 +0300

    second time fixed

--- src/components/CentralBlock.tsx
+++ src/components/CentralBlock.tsx
@@ -18,11 +18,11 @@ const CentralBlock = () => {
     ]
     let [value, setValue] = useState(0)
 
-    const changeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const changeValue = (e: ChangeEvent<HTMLSelectElement>) => {
         setValue(+e.currentTarget.value)
     }
-    let maxValue = 10;
-    let minValue = 0;
+    // let maxValue = 10;
+    // let minValue = 0;
 
     const onClickHandler = () => {
         alert(arr[value])
@@ -30,8 +30,21 @@ const CentralBlock = () => {
 
     return (
         <div className={"body"}>
-            Please, choose any number from 0 to 10 <br/> and get the reason to breake up with this shit: <br/>
-            <input type="number" value={value} onChange={changeValue} max={maxValue} min={minValue}/>
+            Please, choose any number from 1 to 11 <br/> and get the reason to breake up with this shit: <br/>
+            {/*<input type="number" value={value} onChange={changeValue} max={maxValue} min={minValue}/>*/}
+            <select onChange={changeValue}>
+                <option value='0'>1 reason</option>
+                <option value="1">2 reason</option>
+                <option value="2">3 reason</option>
+                <option value="3">4 reason</option>
+                <option value="4">5 reason</option>
+                <option value="5">6 reason</option>
+                <option value="6">7 reason</option>
+                <option value="7">8 reason</option>
+                <option value="8">9 reason</option>
+                <option value="9">10 reason</option>
+                <option value="10">11 reason</option>
+            </select>
             <button onClick={onClickHandler}>Greate choise!!!</button>
         </div>
     );
